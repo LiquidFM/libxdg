0590e7f1db0e8cd00af2dd9980f11233dd580f5f
Dmitriy Vilkov <dav.daemon@gmail.com>
2013-08-11 02:33:12 +0400

    Switch to build environment provided by the platform library.

---
+++ cmake/platform_options.cmake
@@ -0,0 +1,9 @@
+# If you have to link a statically linked library with a shared library,
+# this flag must be set to "YES"
+ENABLE_POSITION_INDEPENDENT_CODE "Enable position independent code"                   0
+
+BUILD_MIME_SPEC                  "Build library with \"Shared MIME-info Database\""   1
+BUILD_DESKTOP_SPEC               "Build library with \"Desktop Entry Specification\"" 1
+BUILD_THEMES_SPEC                "Build library with \"Icon Theme Specification\""    1
+BUILD_MENU_SPEC                  "Build library with \"Desktop Menu Specification\""  1
+BUILD_UPDATE_APPLICATIONS_CACHE  "Build executable for rebuilding the cache"          1
